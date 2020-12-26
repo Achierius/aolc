2f11fe7cd9b3a6c4ce4eb9ba742792ef69b6efb5
Marcus Plutowski <achierius@gmail.com>
2020-12-26 14:58:16 -0800

    Bring mem* functions up to par

--- tests/test_memcpy.cpp
+++ tests/test_memcpy.cpp
@@ -6,26 +6,28 @@
 #include <assert.h>
 #include <stdio.h>
 
-TEST(memcpy, Basic) {
+
+void CompareMemcpyEval(const char* s1, const char* s2, size_t len, const char* comment) {
     using std::placeholders::_1;
     using std::placeholders::_2;
 
+    auto true_fn = std::bind(memcpy, _1, _2, len);
+    auto test_fn = std::bind(_memcpy, _1, _2, len);
+    SCOPED_TRACE(comment);
+    CompareBufferFunctions<void, void, const void>(
+        test_fn, true_fn, s1, s2, len, len);
+}
+
+TEST(memcpy, Basic) {
     char s1[] = {'a', 'a', 'a', 'a', 'a', 's', 's', 's', 's', 's', 's', 'd', 'd', 'd', 'd',
                  'd', 'd', 'd', 'd', 'd', 'd', 'd'};
     char s2[] = {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
                  'x', 'x', 'x', 'x', 'x', 'x', 'x'};
     char s3[] = ":)";
 
-    auto CompareBufferFuncEvalMemcpy = [&](const char* s1, const char* s2, size_t len, const char* comment) {
-        auto true_fn = std::bind(memcpy, _1, _2, len);
-        auto test_fn = std::bind(_memcpy, _1, _2, len);
-        SCOPED_TRACE(comment);
-        CompareBufferFuncEval(test_fn, true_fn, s1, s2, len, len);
-    };
-
-    CompareBufferFuncEvalMemcpy(s1, s2, 20, "s1, s2, 20");
-    CompareBufferFuncEvalMemcpy(s2, s1, 22, "s2, s1, 22");
-    CompareBufferFuncEvalMemcpy(s3, s2, 2,  "s3, s2, 2");
-    CompareBufferFuncEvalMemcpy(s3, s1, 2,  "s3, s1, 2");
-    CompareBufferFuncEvalMemcpy(s1, s2, 4,  "s1, s2, 4");
+    CompareMemcpyEval(s1, s2, 20, "s1, s2, 20");
+    CompareMemcpyEval(s2, s1, 22, "s2, s1, 22");
+    CompareMemcpyEval(s3, s2, 2,  "s3, s2, 2");
+    CompareMemcpyEval(s3, s1, 2,  "s3, s1, 2");
+    CompareMemcpyEval(s1, s2, 4,  "s1, s2, 4");
 }
--- tests/test_memset.cpp
+++ tests/test_memset.cpp
@@ -4,20 +4,20 @@
 #include "aolc/compare_buffer_functions.h"
 #include "gtest/gtest.h"
 
-TEST(memset, Basic) {
+void CompareMemsetEval(const void* dst, const int c, const size_t n, const char* comment) {
     using std::placeholders::_1;
     using std::placeholders::_2;
 
+    auto true_fn = std::bind(memset, _1, c, n);
+    auto test_fn = std::bind(_memset, _1, c, n);
+    SCOPED_TRACE(comment);
+    CompareBufferFunctions<void, void>(test_fn, true_fn, dst, n);
+};
+
+TEST(memset, Basic) {
     char s_true[] = {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
     
-    auto CompareBufferFuncEvalMemset = [&](const void* dst, const int c, const size_t n, const char* comment) {
-        auto true_fn = std::bind(memset, _1, c, n);
-        auto test_fn = std::bind(_memset, _1, c, n);
-        SCOPED_TRACE(comment);
-        CompareBufferFuncEval(test_fn, true_fn, dst, n);
-    };
-
-    CompareBufferFuncEvalMemset(s_true + 3, ' ', 10, "s_true + 3, ' ', 10");
-    CompareBufferFuncEvalMemset(s_true + 0, 'X', 20, "s_true + 0, 'X', 20");
-    CompareBufferFuncEvalMemset(s_true + 13, '\0', 1, "s_true + 13, '\0', 1");
+    CompareMemsetEval(s_true + 3, ' ', 10, "s_true + 3, ' ', 10");
+    CompareMemsetEval(s_true + 0, 'X', 20, "s_true + 0, 'X', 20");
+    CompareMemsetEval(s_true + 13, '\0', 1, "s_true + 13, '\0', 1");
 }
