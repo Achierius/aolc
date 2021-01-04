acf495199563d2c7058d79ccdcf5e2c04027ff65
Marcus Plutowski <achierius@gmail.com>
2021-01-04 14:17:33 -0800

    Add more thorough test cases for strlen

--- tests/test_strlen.cpp
+++ tests/test_strlen.cpp
@@ -1,7 +1,11 @@
 #include "aolc/_test_string.h"
 #include <string.h>
 
-#include <cstring>
+#include <string>
+#include <random>
+#include <array>
+#include <functional>
+#include <algorithm>
 #include "aolc/compare_buffer_functions.h"
 #include "gtest/gtest.h"
 
@@ -51,4 +55,48 @@ TEST(strlen, Basic) {
     EXPECT_EQ(_strlen(s5), strlen(s5));
     EXPECT_EQ(_strlen(s5 + 20), strlen(s5 + 20));
     EXPECT_EQ(_strlen(s6), strlen(s6));
+}
+
+TEST(strlen, Benchmark) {
+    std::array misalignment_factors = {0, 1, 7, 8, 15};
+    std::array misaligned_test_lengths = {8, 32, 256, 2048}; // 4096, 16384}; // Too longer for our test buffers :((
+
+    size_t max_len = *std::max_element(misaligned_test_lengths.begin(), misaligned_test_lengths.end());
+    size_t buf_len = 2*max_len + 1;
+    char* buffer = new char[buf_len];
+    std::uniform_int_distribution<char> char_dist(1, 127); 
+    for (auto off : misalignment_factors) {
+        SCOPED_TRACE("offset: " + std::to_string(off));
+        for (auto len : misaligned_test_lengths) {
+            SCOPED_TRACE("length: " + std::to_string(len));
+            std::mt19937 gen(std::hash<size_t>{}(len));
+            memset(buffer, '\0', buf_len);
+            for (size_t i = off; i < off + len - 1; i++) {
+                buffer[i] = char_dist(gen);
+            }
+
+            CompareStrlenEval(buffer + off, ("strlen len_" + std::to_string(len) +
+                                                   " off_" + std::to_string(off)).c_str());
+
+        }
+    }
+    delete[] buffer;
+}
+
+TEST(strlen, Alignments) {
+    const size_t kBufLen = 128;
+    const size_t kBaseOffset = 0;
+    const size_t kMaxOffset = 64;
+
+    char __attribute__ ((aligned (64))) buffer[kBufLen] = {'\0'}; 
+
+    std::uniform_int_distribution<char> char_dist(1, 127); 
+    std::mt19937 gen(std::hash<size_t>{}(kBufLen));
+    for (size_t i = 0; i < kBufLen - 1; i++) {
+        buffer[i] = char_dist(gen);
+    }
+
+    for (size_t off = kBaseOffset; off < kMaxOffset; off++) {
+        CompareStrlenEval(buffer + off, ("strlen aligned " + std::to_string(off) + " (mod 64)").c_str()); 
+    }
 }
